27494d2bb973a0478b8ac54e99c9e29b79913fc1
sruth <hatakekakashi15903@gmail.com>
2023-07-11 06:45:59 +0530

    backend completed

--- app/src/main/java/com/example/notetaker/plcoding/cleanarchitecturenoteapp/di/AppModule.kt
+++ app/src/main/java/com/example/notetaker/plcoding/cleanarchitecturenoteapp/di/AppModule.kt
@@ -7,6 +7,7 @@ import com.example.notetaker.plcoding.cleanarchitecturenoteapp.feature_note.data
 import com.example.notetaker.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
 import com.example.notetaker.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.AddNoteUseCase
 import com.example.notetaker.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.DeleteNoteUseCase
+import com.example.notetaker.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.GetNoteUeCase
 import com.example.notetaker.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.GetNotesUseCase
 import com.example.notetaker.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.NoteUseCases
 import dagger.Module
@@ -40,7 +41,8 @@ object AppModule {
         return NoteUseCases(
             getNotesUseCase = GetNotesUseCase(repository),
             deleteNoteUseCase = DeleteNoteUseCase(repository),
-            addNoteUseCase = AddNoteUseCase(repository)
+            addNoteUseCase = AddNoteUseCase(repository),
+            getNoteUseCase = GetNoteUeCase(repository)
         )
     }
 }
--- app/src/main/java/com/example/notetaker/plcoding/cleanarchitecturenoteapp/feature_note/presentation/add_edit_notes/AddEditNoteViewModel.kt
+++ app/src/main/java/com/example/notetaker/plcoding/cleanarchitecturenoteapp/feature_note/presentation/add_edit_notes/AddEditNoteViewModel.kt
@@ -3,22 +3,27 @@ package com.example.notetaker.plcoding.cleanarchitecturenoteapp.feature_note.pre
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.toArgb
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.notetaker.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.InvalidNoteExecption
 import com.example.notetaker.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
 import com.example.notetaker.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.NoteUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AddEditNoteViewModel @Inject constructor(
-   private val noteUseCases:NoteUseCases
+   private val noteUseCases:NoteUseCases,
+   savedStateHandle: SavedStateHandle
 ):ViewModel() {
     private val _noteTitle = mutableStateOf(NoteTextFieldState(hint = "Enter the title"))
     val noteTitle: State<NoteTextFieldState> = _noteTitle
 
-    private val _noteContent = mutableStateOf(NoteTextFieldState(hint = "Enter your content"))
+    private val _noteContent = mutableStateOf(NoteTextFieldState(hint = "Enter your content here"))
     val noteContent: State<NoteTextFieldState> = _noteContent
 
     private val _noteColor = mutableStateOf<Int>(Note.noteColors.random().toArgb())
@@ -26,6 +31,79 @@ class AddEditNoteViewModel @Inject constructor(
 
     private val _eventFlow = MutableSharedFlow<UiEvent>()
     val eventFlow = _eventFlow.asSharedFlow()
+
+    private var currentNoteId:Int? = null
+
+    init {
+        savedStateHandle.get<Int>("noteId")?.let{
+            if(it!=-1){
+                viewModelScope.launch {
+                    noteUseCases.getNoteUseCase(it)?.also{ note->
+                        currentNoteId = note.id
+                        _noteTitle.value = noteTitle.value.copy(
+                            text = note.title,
+                            isHIntVisible = false
+                        )
+                        _noteContent.value = noteContent.value.copy(
+                            text = note.title,
+                            isHIntVisible = false
+                        )
+                        _noteColor.value = noteColor.value
+                    }
+                }
+            }
+        }
+    }
+
+    fun onEvent(event:AddEditNoteEvent){
+        when(event){
+            is AddEditNoteEvent.EnteredTitle->{
+                _noteTitle.value = noteTitle.value.copy(
+                    text = event.value
+                )
+            }
+            is AddEditNoteEvent.ChangeTitleFocus ->{
+                _noteTitle.value = noteTitle.value.copy(
+                    isHIntVisible = !event.focusState.isFocused && noteTitle.value.text.isBlank()
+                )
+            }
+            is AddEditNoteEvent.EnteredContent->{
+                _noteContent.value = noteContent.value.copy(
+                    text = event.value
+                )
+            }
+            is AddEditNoteEvent.ChangeContentFocus ->{
+                _noteContent.value = noteContent.value.copy(
+                    isHIntVisible = !event.focusState.isFocused && noteTitle.value.text.isBlank()
+                )
+            }
+            is AddEditNoteEvent.ChangeContentFocus->{
+                _noteColor.value = event.color
+            }
+            is AddEditNoteEvent.SaveNote-> {
+                viewModelScope.launch {
+                    try {
+                        noteUseCases.addNoteUseCase(
+                            Note(
+                                title = noteTitle.value.text,
+                                content = noteContent.value.text,
+                                timeStamp = System.currentTimeMillis(),
+                                color = noteColor.value,
+                                id = currentNoteId
+                            )
+                        )
+                        _eventFlow.emit(UiEvent.SaveNote)
+                    } catch (e: InvalidNoteExecption){
+                        _eventFlow.emit(
+                            UiEvent.ShowSnackbar(
+                                message = e.message?:"Couldn't take note"
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
 
     sealed class UiEvent{
         data class ShowSnackbar(val message:String):UiEvent()
