3dd4c68d8bf691c0064523330263bf53913738b9
Jaswanth-Pillella <jaswanthpillella@gmail.com>
2023-07-24 11:55:57 +0530

    proto 1

--- app/src/main/java/com/example/notetaker/plcoding/cleanarchitecturenoteapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/example/notetaker/plcoding/cleanarchitecturenoteapp/feature_note/presentation/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.notetaker.plcoding.cleanarchitecturenoteapp.feature_note.pre
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             NoteTakerTheme {
                 // A surface container using the 'background' color from the theme
-                androidx.compose.material3.Surface(
+                Surface(
                     color = MaterialTheme.colorScheme.background
                 ){
                     val navController = rememberNavController()
